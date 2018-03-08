d9c6c6011a933117594da13d67b7e2446acc7176
unknown <loganennis@github.com>
2018-03-08 11:54:22 -0800

    Adapter and recycler view added, starting to modify for our project

--- app/src/main/java/com/cpen391group13/inventorymanager/ui/MainMenu.java
+++ app/src/main/java/com/cpen391group13/inventorymanager/ui/MainMenu.java
@@ -84,7 +84,8 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
             fragment = new OverviewFragment();
         }
         else if (id == R.id.nav_warehouses) {
-
+            toolbar.setTitle("Warehouses");
+            fragment = new WarehouseFragment();
         }
         else if (id == R.id.nav_controls) {
             toolbar.setTitle("Test");
